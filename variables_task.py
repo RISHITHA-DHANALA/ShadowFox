3e8984a5c588467f83e74bb21e4c019765a44c63
RISHITHA DHANALA <r92009092@gmail.com>
2026-05-17 15:51:48 +0530

    Finished Python variables practice task

--- variables_task.py
+++ variables_task.py
@@ -1,1 +1,23 @@
+# variable task
 
+a = 22/7
+
+print(a)
+print(type(a))
+
+print("----------------")
+
+# for = 4
+# showing error because for is a keyword
+
+print("for is a keyword so we cannot use it as variable")
+
+print("----------------")
+
+p = 5000
+r = 4
+t = 3
+
+si = (p*r*t)/100
+
+print("Simple Interest is", si)
